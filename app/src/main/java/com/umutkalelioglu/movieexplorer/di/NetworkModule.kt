c1e71c0bb4ed3093624cf71f969e75a402d9c7f5
Umut Kalelioğlu <72888847+umutkaleli@users.noreply.github.com>
2026-01-28 14:21:31 +0300

    feat: setup hilt network module

---
+++ app/src/main/java/com/umutkalelioglu/movieexplorer/di/NetworkModule.kt
@@ -0,0 +1,62 @@
+package com.umutkalelioglu.movieexplorer.di
+
+import com.umutkalelioglu.movieexplorer.BuildConfig
+import com.umutkalelioglu.movieexplorer.common.Constants
+import com.umutkalelioglu.movieexplorer.data.remote.MovieApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor { chain ->
+                // Her isteğe API Key'i otomatik ekleyen Interceptor
+                val original = chain.request()
+                val originalHttpUrl = original.url
+
+                val url = originalHttpUrl.newBuilder()
+                    .addQueryParameter("api_key", BuildConfig.API_KEY)
+                    .build()
+
+                val requestBuilder = original.newBuilder().url(url)
+                chain.proceed(requestBuilder.build())
+            }
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieApi(retrofit: Retrofit): MovieApi {
+        return retrofit.create(MovieApi::class.java)
+    }
+}
